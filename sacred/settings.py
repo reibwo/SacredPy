cc9baba9ae3a5470cd774f17c934617dc19a6be6
Jason Myers <jason@jasonamyers.com>
2013-10-28 20:55:33 -0500

    Changing Databases
    Signed-off-by: Jason Myers <jason@jasonamyers.com>

--- sacred/settings.py
+++ sacred/settings.py
@@ -19,9 +19,10 @@ ADMINS = (
 )
 
 MANAGERS = ADMINS
-# Parse database configuration from $DATABASE_URL
 
+# Parse database configuration from $DATABASE_URL
 import dj_database_url
+DATABASES = {}
 DATABASES['default'] =  dj_database_url.config()
 
 #DATABASES = {
